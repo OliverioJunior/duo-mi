343ef7bd254cf83e621ce9566e9babfa4a2bbc50
Oliverio <oliverio.junior2@gmail.com>
2022-01-04 10:39:16 -0300

    Adicionar propriedades flex

--- src/components/telas/cantoSuperiorEsquerdo/Tela-01.js
+++ src/components/telas/cantoSuperiorEsquerdo/Tela-01.js
@@ -1,12 +1,12 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./Tela-01.css"
 
 const Tela_01 = () => {
 
-    window.onload = () =>{
-      
+    window.onload = () => {
+
     }
-   
+
 
 
 
@@ -14,62 +14,41 @@ const Tela_01 = () => {
     return (
 
         <div className='cantoSuperiorEsquerdo'>
-
-            <div className="Emily">
-                <div className ="adesivo"/>
-                <div className="balao"/>
-                <div className="seta_curva"/>
-            </div>
-                <div className="plantinha"/>
-                    
-            
-            <div className="modal"/>
-            <div className="emilyNome">
-                <div className="emilyE" >
-
+            <div className="container">
+                <div className="Emily">
+                    <div className="adesivo" />
+                    <div className="balao" />
+                    <div className="seta_curva" />
                 </div>
-                <div className="emilyM" >
-
+                <div className="plantinha" />
+                <div className="modal" />
+                
+                <div className="container-2">
+                <div className="emilyNome">
+                    <div className="emilyE" />
+                    <div className="emilyM" />
+                    <div className="emilyI" />
+                    <div className="emilyL" />
+                    <div className="emilyY" />
                 </div>
-                <div className="emilyI" >
-
+                <div className='imagemTexto' />
                 </div>
-                <div className="emilyL" >
-
                 </div>
-                <div className="emilyY" >
-
-                </div>
-            </div>
-            <div className='fotoDupla'>
-
-            </div>
-            <div className='imagemTexto'>
-
-            </div>
-            <div className='tamis'>
-
-            </div>
+           
+            <div className="container-3">
+            <div className='fotoDupla'/>
+            <div className='tamis'/>
             <div className='tamisNome'>
-                <div className='tamisT'>
-
-                </div>
-                <div className='tamisA' >
-
-                </div>
-                <div className='tamisM'>
-
-                </div>
-                <div className='tamisI'>
-
-                </div>
-                <div className='tamisS'>
-
-                </div>
+                <div className='tamisT'/>
+                <div className='tamisA'/>
+                <div className='tamisM'/>
+                <div className='tamisI'/>
+                <div className='tamisS'/>
             </div>
-        </div>
+            </div>
 
-    )
+        </div>
+     )
 }
 
 export default Tela_01
