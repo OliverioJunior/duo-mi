5281cc26c8af5114fd8132818461d321f2ce43d9
Oliverio <oliverio.junior2@gmail.com>
2022-01-06 11:38:02 -0300

    finalizar tela inferior direita

--- src/components/telas/cantoInferiorDireito/Tela-09.js
+++ src/components/telas/cantoInferiorDireito/Tela-09.js
@@ -5,6 +5,12 @@ import "./Tela-09.css"
 function Tela_09() {
     return (
         <div className='cantoInferiorDireito'>
+            <div className='plantaVerdeTv'/>
+            <div className='plantaRosaTv'/>
+            <div className='televisor'>
+                <div className='botaoPlay'/>
+                <div className='cursor'/>
+            </div>   
             
         </div>
     )
