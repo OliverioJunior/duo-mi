a7928a188cc0f66bea8c828df7216f8336f30518
Oliverio <oliverio.junior2@gmail.com>
2022-01-10 19:51:07 -0300

    tela inferior direita

--- op.js
+++ op.js
@@ -1,11 +1,8 @@
-/*let x = 624 --- 500
-          641 ---  x*/
-let y 
+
 
 function regra3(a,b,c){
     
    return b*c/a
 }
 5
-console.log(regra3(403,293,257))
-
+console.log(regra3(257,186,200))
--- src/components/telas/cantoInferiorDireito/Tela-09.js
+++ src/components/telas/cantoInferiorDireito/Tela-09.js
@@ -4,15 +4,20 @@ import "./Tela-09.css"
 
 function Tela_09() {
     return (
+        
         <div className='cantoInferiorDireito'>
+            
             <div className='plantaVerdeTv'/>
             <div className='plantaRosaTv'/>
             <div className='televisor'>
+                <span className='fraseTv'> clique na <span className='filhaTv'>tv</span> para acessar a
+                <span className="fraseFilha"> galeria de jobs</span></span>
                 <div className='botaoPlay'/>
                 <div className='cursor'/>
             </div>   
             
         </div>
+        
     )
 }
 export default Tela_09;
