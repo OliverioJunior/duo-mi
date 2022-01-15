a88e820e14c022e034e409645e8b0246f3208637
Oliverio <oliverio.junior2@gmail.com>
2022-01-15 00:35:08 -0300

    Descolapsar tela 09

--- src/components/telas/cantoInferiorDireito/Tela-09.js
+++ src/components/telas/cantoInferiorDireito/Tela-09.js
@@ -6,16 +6,16 @@ function Tela_09() {
     return (
         
         <div className='cantoInferiorDireito'>
-            
-            <div className='plantaVerdeTv'/>
-            <div className='plantaRosaTv'/>
-            <div className='televisor'>
-                <span className='fraseTv'> clique na <span className='filhaTv'>tv</span> para acessar a
-                <span className="fraseFilha"> galeria de jobs</span></span>
-                <div className='botaoPlay'/>
-                <div className='cursor'/>
-            </div>   
-            
+            <div className='tvSemColapsar'>
+                <div className='plantaVerdeTv'/>
+                <div className='plantaRosaTv'/>
+                <div className='televisor'>
+                    <span className='fraseTv'> clique na <span className='filhaTv'>tv</span> para acessar a
+                    <span className="fraseFilha"> galeria de jobs</span></span>
+                    <div className='botaoPlay'/>
+                    <div className='cursor'/>
+                </div>   
+            </div>
         </div>
         
     )
