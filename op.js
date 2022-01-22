36443b84ad48c02d8e320555087b8c1b3f030c30
Oliverio <oliverio.junior2@gmail.com>
2022-01-22 14:55:36 -0300

    Finalizar função de locomoção

--- op.js
+++ op.js
@@ -1,9 +1,9 @@
 
-
+/* 
 function regra3(a,b,c){
     
    return b*c/a
 }
 5
 console.log(regra3(140,101,100))
- 
+  */
--- src/components/BackGround.js
+++ src/components/BackGround.js
@@ -14,7 +14,7 @@ function BackGround() {
     const [button, setButton] = useState(true);
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
-    
+   
     const mouse = {
         ativo: false,
         movendo: false,
@@ -22,59 +22,80 @@ function BackGround() {
               y:0},
         posAnterior: {x:0, 
             y:0},
+        posClick: {x:0,
+        y:0},
+        savePosicao:{x:0,y:0}
     }
 
+
     
-    window.addEventListener("mousedown", (e) => {mouse.ativo = true
-        if(mouse.ativo){
-            mouse.posAnterior.x = e.clientX
-            mouse.posAnterior.y = e.clientY
-        }
-        let button = e
-        console.log(button)
-    })
-    window.addEventListener("mouseup", (e) => {mouse.ativo = false 
-        mouse.movendo = false
+    window.addEventListener("mousedown", (e) => {
         
-        })
+        mouse.ativo = true
+        mouse.movendo = true
+        mouse.posClick.x = e.clientX
+        mouse.posClick.y = e.clientY
+        console.log(e)
+       
+    })
     window.addEventListener("mousemove", (e)=>{
+        if(mouse.ativo && mouse.movendo){
+            
+            mouse.posAnterior.x = mouse.posClick.x - e.clientX;
+            mouse.posAnterior.y = mouse.posClick.y - e.clientY;
+            console.log(mouse.posAnterior.x)
+            setTimeout(mover(),300) 
+          }
+      })
+
+
+        function mover(){
+          if(mouse.ativo && mouse.movendo){
+              mouse.pos.y = (mouse.savePosicao.y - mouse.posAnterior.y)*(-0.8)
+              mouse.pos.x = (mouse.savePosicao.x - mouse.posAnterior.x)*(-0.8)
+              window.scroll({top:mouse.pos.y, left:mouse.pos.x})
+           
+              
+           }
+       } 
+
+       window.addEventListener("mouseup", (e) => {
+        mouse.ativo = false;
+        mouse.movendo = false;
+        mouse.savePosicao.x += mouse.posAnterior.x*(-1)
+        mouse.savePosicao.y += mouse.posAnterior.y*(-1)
+      }) 
+
+     /* window.addEventListener("mousemove", function ciclo(e){
         
         if(mouse.ativo){
             mouse.movendo = true
         }
         if(mouse.ativo && mouse.movendo){
-            mouse.pos.x += mouse.posAnterior.x - e.clientX;
-            mouse.pos.y += mouse.posAnterior.y - e.clientY;
-        }
-         
-    })
-
-    window.addEventListener('mousemove', ()=>{
-        if(mouse.ativo && mouse.movendo == true){
-            window.moveTo(1500, 1500)
-           
+            mouse.posAnterior.x = mouse.posClick.x - e.clientX;
+            mouse.posAnterior.y = mouse.posClick.y - e.clientY;
+            mouse.moveu.y = e.clientY;
+            mouse.moveu.x = e.clientX;
+            console.log(mouse.posAnterior.x)
         }
         
-    })
-  /*   const moverTela = ()=>{
-        window.scrollTo({
-            top: mouse.pos.x,
-            left: mouse.pos.y,
-            behavior: "smooth"
-        })
-    }
-    const ciclo = ()=>{
-        if(mouse.ativo && mouse.movendo && mouse.posAnterior){
-            moverTela({pos:mouse.pos, posAnterior:mouse.posAnterior})
-            mouse.movendo = false;
-        }
-        mouse.posAnterior = {x: mouse.pos.x, y: mouse.pos.y}
-        setTimeout(ciclo, 10);
-    }
-    window.addEventListener("mousedown", ciclo())
+    }) 
+    let mover = 0
+     window.addEventListener('mousemove', ()=>{
+        mover = setTimeout(()=>{
+            if(mouse.ativo && mouse.movendo){
+                mouse.pos.x = mouse.posAnterior.x - mouse.moveu.x
+                mouse.pos.y = mouse.posAnterior.y - mouse.moveu.y
+                console.log(mouse.pos.x)
+                window.scrollTo({top:mouse.pos.y, left:mouse.pos.x, behavior: "smooth"})
+            } else {
+                return
+            }
+            
+        },10)   
+    }) 
+  
  */
-
-
 
 
 
@@ -86,6 +107,7 @@ function BackGround() {
         } else {
             setButton(true)
         }
+        
     };
 
     window.addEventListener('resize', showButton);
