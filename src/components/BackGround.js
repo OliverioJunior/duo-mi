ca740a0084c3aff907669d6afe5124d000668110
Oliverio <oliverio.junior2@gmail.com>
2022-02-16 06:20:50 -0300

    consertar bug da tela 09

--- src/components/BackGround.js
+++ src/components/BackGround.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from 'react'
 import { Link } from 'react-router-dom'
 import { Button } from './Button.js';
 import './BackGround.css'
-import {Tela_01 , Tela_03 , Tela_05, Tela_07, Tela_09} from './telas/index.js';
+import { Tela_01, Tela_03, Tela_05, Tela_07, Tela_09 } from './telas/index.js';
 
 
 
@@ -15,21 +15,27 @@ function BackGround() {
     const mouse = {
         ativo: false,
         movendo: false,
-        pos: {x:0, 
-              y:0},
-        posAnterior: {x:0, 
-            y:0},
-        posClick: {x:0,
-        y:0},
-        savePosicao:{x:0,y:0}
+        pos: {
+            x: 0,
+            y: 0
+        },
+        posAnterior: {
+            x: 0,
+            y: 0
+        },
+        posClick: {
+            x: 0,
+            y: 0
+        },
+        savePosicao: { x: 0, y: 0 }
     }
-    function slideScreen(){
-        if(mouse.ativo){
-            mouse.pos.y = (mouse.savePosicao.y - mouse.posAnterior.y)*(-0.8)
-            mouse.pos.x = (mouse.savePosicao.x - mouse.posAnterior.x)*(-0.8)
-            window.scroll({top:mouse.pos.y, left:mouse.pos.x})
-         }
-     } 
+    function slideScreen() {
+        if (mouse.ativo) {
+            mouse.pos.y = (mouse.savePosicao.y - mouse.posAnterior.y) * (-0.8)
+            mouse.pos.x = (mouse.savePosicao.x - mouse.posAnterior.x) * (-0.8)
+            window.scroll({ top: mouse.pos.y, left: mouse.pos.x })
+        }
+    }
 
 
     window.addEventListener("mousedown", (e) => {
@@ -38,65 +44,65 @@ function BackGround() {
         mouse.posClick.y = e.clientY
         body.style.cursor = "grabbing"
     })
-    window.addEventListener("mousemove", (e)=>{
+    window.addEventListener("mousemove", (e) => {
         e.preventDefault()
-        if(mouse.ativo){
+        if (mouse.ativo) {
             mouse.posAnterior.x = mouse.posClick.x - e.clientX;
             mouse.posAnterior.y = mouse.posClick.y - e.clientY;
-            setTimeout(slideScreen(),300) 
-          }
-      })
-
-
-       
-
-       window.addEventListener("mouseup", () => {
-        mouse.ativo = false;
-        mouse.savePosicao.x +=  mouse.posAnterior.x*(-1);
-        mouse.savePosicao.y +=  mouse.posAnterior.y*(-1);
-        body.style.cursor = "grab";
-      }) 
-
-   /*  const canvasRef = useRef(null) 
-    const contextRef = useRef(null) 
-    const [isDrawing, setIsDrawing] = useState(false)
-    useEffect(() => {
-    const canvas = canvasRef.current;
-   
-    const context = canvas.getContext('2d');
-    context.scale(2,2);
-    context.lineCap = "round";
-    context.strokeStyle = "red";
-    context.lineWidth = 1;
-    contextRef.current = context;
-    }, [])
-    const startDrawing = (e) =>{
-        setIsDrawing(true);
-        const {clientX,clientY} = e;
-        contextRef.current.beginPath();
-        contextRef.current.moveTo(clientX,clientY);
-        console.log(canvasRef.current)
-    }
-    const finishDrawing = () =>{
-        contextRef.current.closePath()
-        setIsDrawing(false)
-    }
-    const draw = (e) =>{
-        const {clientX,clientY} = e;
-       if(isDrawing){ 
-           contextRef.current.lineTo(clientX,clientY)
-           contextRef.current.stroke()
+            setTimeout(slideScreen(), 300)
         }
-    } */
+    })
 
-     
+
+
+
+    window.addEventListener("mouseup", () => {
+        mouse.ativo = false;
+        mouse.savePosicao.x += mouse.posAnterior.x * (-1);
+        mouse.savePosicao.y += mouse.posAnterior.y * (-1);
+        body.style.cursor = "grab";
+    })
+
+    /*  const canvasRef = useRef(null) 
+     const contextRef = useRef(null) 
+     const [isDrawing, setIsDrawing] = useState(false)
+     useEffect(() => {
+     const canvas = canvasRef.current;
+    
+     const context = canvas.getContext('2d');
+     context.scale(2,2);
+     context.lineCap = "round";
+     context.strokeStyle = "red";
+     context.lineWidth = 1;
+     contextRef.current = context;
+     }, [])
+     const startDrawing = (e) =>{
+         setIsDrawing(true);
+         const {clientX,clientY} = e;
+         contextRef.current.beginPath();
+         contextRef.current.moveTo(clientX,clientY);
+         console.log(canvasRef.current)
+     }
+     const finishDrawing = () =>{
+         contextRef.current.closePath()
+         setIsDrawing(false)
+     }
+     const draw = (e) =>{
+         const {clientX,clientY} = e;
+        if(isDrawing){ 
+            contextRef.current.lineTo(clientX,clientY)
+            contextRef.current.stroke()
+         }
+     } */
+
+
     const showButton = () => {
         if (window.innerWidth <= 960) {
             setButton(false)
         } else {
             setButton(true)
         }
-        if(mouse.ativo){
+        if (mouse.ativo) {
             setClick(true)
         } else {
             setClick(false)
@@ -104,33 +110,33 @@ function BackGround() {
     };
 
     window.addEventListener('resize', showButton);
+    window.addEventListener('loadstart',(e)=>{
+       e.scrollTo(3000,3000)
+    });
 
     return (
         <>
-
+           
             <div className='back-ground'>
 
-               <Tela_01/>
-                <div className="cantoMeioEsquerdo"/>
-                <Tela_07  /* onMouseDown={startDrawing}
-                    onMouseUp={finishDrawing}
-                    onMouseMove={draw}
-                    ref = {canvasRef} *//>
+                <Tela_01 />
+                <div className="cantoMeioEsquerdo" />
+                <Tela_07 />
                 <div className="meioSuperior">
                 </div>
 
-                <Tela_05/>
+                <Tela_05 />
 
 
 
                 <div className="meioInferior">
 
                 </div>
-                <Tela_03/>
+                <Tela_03 />
                 <div className="cantoMeioDireito">
 
                 </div>
-                <Tela_09/>
+              {/*   <Tela_09 /> */}
             </div>
 
 
