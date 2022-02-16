06915a91e6387c231dae647f4bd5945d14cd1082
Oliverio <oliverio.junior2@gmail.com>
2022-02-15 21:39:34 -0300

    adicionar rotação nas fotos

--- src/components/telas/cantoSuperiorEsquerdo/fotoEmily/FotoEmily.js
+++ src/components/telas/cantoSuperiorEsquerdo/fotoEmily/FotoEmily.js
@@ -1,39 +1,34 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import "./FotoEmily.css"
 
 
 
 function FotoEmily() {
-    const [click,setClick] = useState(true);
-     const Virar = (e)=>{
-        setClick(!click)
-    
-      
-      /* if(!click){
-
-          e.target.style.transform = "rotateY(180deg)"
-         
-
-      } else {
-        e.target.style.transform = "rotateY(0deg)"
-      } */
-        
-    } 
-      window.addEventListener('resize',(e)=>{
-        console.log(e.target)
-    }) 
-    return (
-        <>
-                <div className={click ? "girar":"girando"} onClick={Virar}>
-                    <div className="Emily">
-                        <div className="adesivo" />
-                        <div className="balao" />
-                        <div className="seta_curva" />
-                    </div>
-                    <div className='fundo'/>
-                </div>
-        </>
-    )
+  const [click, setClick] = useState(true);
+  const Virar = () => {
+    setClick(!click)
+    let flip = document.querySelector('.emilyContainer')
+    if (click) {
+      flip.style.transform = "rotate(-2deg) rotateY(180deg)"
+    } else {
+      flip.style.transform = "rotate(-2deg) rotateY(0deg)"
+    }
+  }
+  window.addEventListener('resize', (e) => {
+    console.log(e.target)
+  })
+  return (
+    <>
+      <div className="emilyContainer" onClick={Virar}>
+        <div className="Emily">
+          <div className="adesivo" />
+          <div className="balao" />
+          <div className="seta_curva" />
+        </div>
+        <div className='fundo' />
+      </div>
+    </>
+  )
 }
 
 export default FotoEmily;
