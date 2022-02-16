91892cec3235895e344cc97edef5d5e8b9c659b6
Oliverio <oliverio.junior2@gmail.com>
2022-02-16 06:21:27 -0300

    tirar fundo que aparece na hora que não deveria

--- src/components/telas/cantoSuperiorEsquerdo/fotoTamis/FotoTamis.js
+++ src/components/telas/cantoSuperiorEsquerdo/fotoTamis/FotoTamis.js
@@ -2,25 +2,34 @@ import React, { useState } from 'react';
 import "./FotoTamis.css";
 
 function FotoTamis() {
-    const [click,setClick] = useState(true);
+    const [click, setClick] = useState(true);
     const girar = (e) => {
         setClick(!click)
-        let flip = document.querySelector(".tamisFrente")
-        if(click){
+        let flip = document.querySelector(".containerTamis")
+        if (click) {
             flip.style.transform = "rotate(-2deg) rotateY(180deg)"
         } else {
             flip.style.transform = "rotate(-2deg) rotateY(0deg)"
         }
     }
-  return (
-    <div className = "tamisFrente"
-    onClick={girar}>
-        <div className='tamis'>
-            <div className="tamiSeta" />
-        </div>
-        <div className="tamisDescricao"/>
-    </div>
-  );
+    return (
+        <>
+            <div className="containerTamis"
+                onClick={girar}>
+                <div className='tamis'>
+                    <div className="tamiSeta" />
+                </div>
+                <div className="tamisDescricao" />
+            </div>
+            <div className='tamisNome'>
+                <div className='tamisT' />
+                <div className='tamisA' />
+                <div className='tamisM' />
+                <div className='tamisI' />
+                <div className='tamisS' />
+            </div>
+        </>
+    );
 };
 
 export default FotoTamis
