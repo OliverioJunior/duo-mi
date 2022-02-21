9f94f42907cfef28cb6774f8a57f6c0a2b2c1995
Oliverio <oliverio.junior2@gmail.com>
2022-02-21 06:19:58 -0300

    Criar tela-02

--- src/components/telas/index.js
+++ src/components/telas/index.js
@@ -1,7 +1,8 @@
 import Tela_01 from "./cantoSuperiorEsquerdo/Tela-01"
+import Tela_02 from "./meioSuperior/Tela-02"
 import Tela_03 from "./cantoSuperiorDireito/Tela-03";
 import Tela_05 from "./meio/Tela-05";
 import Tela_07 from "./cantoInferiorEsquerdo/Tela-07";
 import Tela_09 from "./cantoInferiorDireito/Tela-09";
 
-export {Tela_01, Tela_03, Tela_05, Tela_07, Tela_09};
+export {Tela_01, Tela_02, Tela_03, Tela_05, Tela_07, Tela_09};
---
+++ src/components/telas/meioSuperior/Tela-02.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./Tela-02.css";
+
+function Tela_02(){
+    return(
+        <div className='meioSuperior'>
+           <div className='meioFrase'>
+                <h1>Acompanhe-nos:</h1>
+           </div> 
+           <div className="redesSociais">
+                <a className='instagram'></a>
+                <a className='behance'></a>
+                <a className='facebook'></a>
+                <a className='tiktok'></a>
+            </div>
+        </div>
+    )
+}
+
+export default Tela_02;
