f790ad64c2a43ae511cb81610a1c371939e1c92d
Oliverio <oliverio.junior2@gmail.com>
2022-03-02 10:29:57 -0300

    Adicionar links de navegação

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,75 +1,83 @@
-import React, {useState} from 'react'
-import {Link} from 'react-router-dom'
-import { Button } from './Button.js';
+import React, { useState } from 'react'
 import './Navbar.css'
+
+let resTop = 0
+let resLeft = 0
+let setTela = false;
+window.addEventListener('click', () => {
+    const menu = document.querySelectorAll('.nav-menu a')
+    menu.forEach(i => {
+        i.addEventListener('click', (e) => {
+            e.preventDefault()
+            const element = e.target;
+            const id = element.getAttribute('href');
+            const Top = document.querySelector(id).offsetTop;
+            const Left = document.querySelector(id).offsetLeft;
+            let resLeft = Left
+            let resTop = Top
+            window.scrollTo({
+                top: resTop,
+                left: resLeft,
+                behavior: 'smooth'
+            })
+           setTela = true;
+        })
+    })
+})
+export {resLeft, setTela};
+export {resTop};
 
 function Navbar() {
     const [click, setClick] = useState(false);
-    const [button,setButton] = useState(true);
+    const [button, setButton] = useState(true);
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
 
-    const showButton = () => {
-        if(window.innerWidth<=960){
-            setButton(false)
-        } else {
-            setButton(true)
-        }
-    };
+    /*    const showButton = () => {
+           if(window.innerWidth<=960){
+               setButton(false)
+           } else {
+               setButton(true)
+           }
+       }; */
 
-    window.addEventListener('resize', showButton);
-    window.addEventListener('click',()=>{
-        const menu = document.querySelectorAll('.nav-menu a')
-        menu.forEach(i=>{
-            i.addEventListener('click',(e)=>{
-                e.preventDefault()
-                const element = e.target;
-                const id = element.getAttribute('href');
-                const to = document.querySelector(id).offsetTop;
-                console.log(id)
-                window.scrollTo({
-                    top: id,
-                    left:id,
-                    behavior:'smooth'
-                })
-            })
-        })
-    })
+    /* window.addEventListener('resize', showButton); */
+
     return (
-        <> 
-        <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet'></link>
-        <nav className='navbar'>
-            <div className='navbar-container'>
-               
-                <div onClick={handleClick} className='menu-icon' >
-                    <i className={click ? 'fas fa-times' : 'fas fa-paper-plane'}></i>
+        <>
+            <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet'></link>
+            <nav className='navbar'>
+                <div className='navbar-container'>
+
+                    <div onClick={handleClick} className='menu-icon' >
+                        <i className={click ? 'fas fa-times' : 'fas fa-paper-plane'}></i>
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item' >
+                            <a href='#quemsomos' className='nav-links' onClick={closeMobileMenu}>
+                                Quem somos
+                            </a>
+                        </li>
+                        <li className='nav-item' >
+                            <a href='#jobs' className='nav-links' onClick={closeMobileMenu}>
+                                Nossos Jobs
+                            </a>
+                        </li>
+                        <li className='nav-item' >
+                            <a href='#atendimento' className='nav-links' onClick={closeMobileMenu}>
+                                Atendimento Online
+                            </a>
+                        </li>
+
+                    </ul>
+
+                    <div>
+
+                    </div>
                 </div>
-                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                    <li className='nav-item' >
-                        <a href='#quemsomos' className='nav-links' onClick={closeMobileMenu}>
-                        Quem somos
-                        </a>
-                    </li>
-                    <li className='nav-item' >
-                        <a href='#jobs' className='nav-links' onClick={closeMobileMenu}>
-                            Nossos Jobs
-                        </a>
-                    </li>
-                    <li className='nav-item' >
-                        <a href='#atendimento' className='nav-links' onClick={closeMobileMenu}>
-                           Atendimento Online
-                        </a>
-                    </li>
-                    
-                </ul>
-               
-                <div>
-     
-    </div>
-            </div>
-        </nav>
+            </nav>
         </>
     )
 }
 
-export default Navbar
+export {Navbar};
