6fc227629ce76714917b749cc3a26d2df2505442
Oliverio <oliverio.junior2@gmail.com>
2022-03-24 20:17:07 -0300

    adicionar links nas tag's de links

--- src/components/telas/meioSuperior/Tela-02.js
+++ src/components/telas/meioSuperior/Tela-02.js
@@ -8,10 +8,10 @@ function Tela_02(){
                 <h1>Acompanhe-nos:</h1>
            </div> 
            <div className="redesSociais">
-                <a className='instagram'></a>
-                <a className='behance'></a>
-                <a className='facebook'></a>
-                <a className='tiktok'></a>
+                <a href='https://www.instagram.com/duomi.estudiocriativo/' target="_blank" className='instagram'></a>
+                <a href='https://www.behance.net/estudioduomi?tracking_source=search_projects%7Cduomi%20' target ='_blank' class='behance'></a>
+                <a href='https://www.facebook.com/DuoM%C3%AD-Est%C3%BAdio-Criativo-105370734736231/' target ='_blank' className='facebook'></a>
+                <a href='https://www.tiktok.com/@duomi.estudiocriativo' target ='_blank' className='tiktok'></a>
             </div>
         </div>
     )
