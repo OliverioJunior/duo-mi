d632297529d18af89c387faf5367cc39cf50aa29
Oliverio <oliverio.junior2@gmail.com>
2022-03-26 18:00:56 -0300

    Adicionar rel=' noreferrer'

--- src/components/telas/meioSuperior/Tela-02.js
+++ src/components/telas/meioSuperior/Tela-02.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
 import React from 'react';
 import "./Tela-02.css";
 
@@ -8,10 +9,10 @@ function Tela_02(){
                 <h1>Acompanhe-nos:</h1>
            </div> 
            <div className="redesSociais">
-                <a href='https://www.instagram.com/duomi.estudiocriativo/' target="_blank" className='instagram'></a>
-                <a href='https://www.behance.net/estudioduomi?tracking_source=search_projects%7Cduomi%20' target ='_blank' class='behance'></a>
-                <a href='https://www.facebook.com/DuoM%C3%AD-Est%C3%BAdio-Criativo-105370734736231/' target ='_blank' className='facebook'></a>
-                <a href='https://www.tiktok.com/@duomi.estudiocriativo' target ='_blank' className='tiktok'></a>
+                <a href='https://www.instagram.com/duomi.estudiocriativo/' rel="noreferrer" target="_blank" className='instagram'></a>
+                <a href='https://www.behance.net/estudioduomi?tracking_source=search_projects%7Cduomi%20' rel="noreferrer"  target ='_blank' class='behance'></a>
+                <a href='https://www.facebook.com/DuoM%C3%AD-Est%C3%BAdio-Criativo-105370734736231/' rel="noreferrer"  target ='_blank' className='facebook'></a>
+                <a href='https://www.tiktok.com/@duomi.estudiocriativo' target ='_blank' rel="noreferrer"  className='tiktok'></a>
             </div>
         </div>
     )
