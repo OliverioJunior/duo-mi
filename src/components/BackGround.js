d24e4a069449942f4c4408648f8d57f72139ab1b
Oliverio <oliverio.junior2@gmail.com>
2022-03-26 18:00:03 -0300

    Melhorar função de locomoção pela tela

--- src/components/BackGround.js
+++ src/components/BackGround.js
@@ -1,157 +1,60 @@
 /* eslint-disable react/jsx-pascal-case */
-import React, { useState } from 'react'
-import './BackGround.css'
+import React from 'react';
+import './BackGround.css';
 import { Tela_01, Tela_02, Tela_03, Tela_07, Tela_09 } from './telas/index.js';
 import  Tela_05  from './telas/meio/Tela-05';
 import Globais from './Globais';
-import { Navbar } from './Navbar';
 
 
 function  BackGround() {
-    
-    const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true);
-    /* const handleClick = () => setClick(!click);
-    const closeMobileMenu = () => setClick(false); */
-    const body = document.querySelector("body")
+    const body = document.querySelector('body');
     const mouse = {
-        ativo: false,
-        movendo: false,
-        desenhando: false,
-        pos: {
+        x: 0,
+        y: 0,
+        mouseMove: false,
+        savePosition: {
             x: 0,
             y: 0
         },
-        posAnterior: {
-            x: 0,
-            y: 1000
-        },
-        posClick: {
-            x: 0,
-            y: 0
-        },
-        savePosicao: { x: Globais.resTop, y: Globais.resLeft}
-    }
-    function slideScreen() {
-       
-        if (mouse.ativo && mouse.desenhando !== true) {
-            mouse.pos.y = (mouse.savePosicao.y - mouse.posAnterior.y)*(-1) 
-            mouse.pos.x = (mouse.savePosicao.x - mouse.posAnterior.x)*(-1) 
-            window.scroll({ top: mouse.pos.y, left: mouse.pos.x , behavior: 'smooth' })
-        }
-        console.log('SlideScreen='+mouse.savePosicao.x)
-    }
-
-   /*  window.addEventListener('scroll',(e)=>{
-        console.log('scrolling')
-        console.log(e)
-    }) */
-    window.addEventListener("mousedown", (e) => {
-        if(Globais.setTela && mouse.desenhando !== true){
-            console.log('Restop =' + Globais.resTop)
-            console.log('PosAnterior ='+mouse.savePosicao.x)
-            mouse.savePosicao.x = Globais.resTop
-            mouse.savePosicao.y = Globais.resLeft
-            Globais.setTela = false
-            console.log('PosAnteriorNov='+mouse.savePosicao.x)
-        }
-        mouse.ativo = true
-        mouse.posClick.x = e.clientX
-        mouse.posClick.y = e.clientY
-        body.style.cursor = "grabbing"
-       
-       
-        
-    })
-    window.addEventListener("mousemove", (e) => {
-        if(Globais.setTela){
-            return
-        }
-        e.preventDefault()
-         if (mouse.ativo && mouse.desenhando !== true) {
-            mouse.posAnterior.x = mouse.posClick.x - e.clientX;
-            mouse.posAnterior.y = mouse.posClick.y - e.clientY;
-            setTimeout(slideScreen(), 150)
-        }
-    })
-    
-    window.addEventListener("mouseup", () => {
-        if(mouse.savePosicao.x > 0){
-            mouse.savePosicao.x = 0
-        }
-        if(mouse.savePosicao.y > 0){
-            mouse.savePosicao.y = 0
-        }
-        
-       
-        if(mouse.ativo && mouse.desenhando !== true){
-        mouse.ativo = false;
-        mouse.savePosicao.x += mouse.posAnterior.x * (-1);
-        mouse.savePosicao.y += mouse.posAnterior.y * (-1);
-        console.log('MouseUp='+mouse.savePosicao.x)
-        body.style.cursor = "grab";     
-        }
-    })
-
-    /*  const canvasRef = useRef(null) 
-     const contextRef = useRef(null) 
-     const [isDrawing, setIsDrawing] = useState(false)
-     useEffect(() => {
-     const canvas = canvasRef.current;
-    
-     const context = canvas.getContext('2d');
-     context.scale(2,2);
-     context.lineCap = "round";
-     context.strokeStyle = "red";
-     context.lineWidth = 1;
-     contextRef.current = context;
-     }, [])
-     const startDrawing = (e) =>{
-         setIsDrawing(true);
-         const {clientX,clientY} = e;
-         contextRef.current.beginPath();
-         contextRef.current.moveTo(clientX,clientY);
-         console.log(canvasRef.current)
-     }
-     const finishDrawing = () =>{
-         contextRef.current.closePath()
-         setIsDrawing(false)
-     }
-     const draw = (e) =>{
-         const {clientX,clientY} = e;
-        if(isDrawing){ 
-            contextRef.current.lineTo(clientX,clientY)
-            contextRef.current.stroke()
-         }
-     } */
-
-
-    const showButton = () => {
-        if (window.innerWidth <= 960) {
-            setButton(false)
-        } else {
-            setButton(true)
-        }
-        if (mouse.ativo) {
-            setClick(true)
-        } else {
-            setClick(false)
-        }
+        drawing: false,
     };
-    
-   /*  primeiroLogin.addEventListener('click', (e) => {
-        e.preventDefault()
-        const element = e.target;
-        const id = element.getAttribute('href');
-        Top = document.querySelector(id).offsetTop;
-        Left = document.querySelector(id).offsetLeft;
+  window.addEventListener('mousedown',(e)=>{
+      if(mouse.drawing === false){
+            Globais.resRight =  window.scrollX;
+            Globais.resTop = window.scrollY;
+            mouse.x = e.clientX;
+            mouse.y = e.clientY;
+            mouse.mouseMove = true;
+            body.style.cursor = 'grabing';
+    }
+  })
+  window.addEventListener('mouseup',()=>{
+      if(mouse.drawing === false){
+            mouse.mouseMove = false;
+            mouse.savePosition.x = window.scrollX;
+            mouse.savePosition.y = window.scrollY;
+            body.style.cursor = 'grab';
+    }
+  })
+  window.addEventListener('mousemove',(e)=>{
+      //to take the mouse position
+      //prevent mouse comportament 
+    e.preventDefault();
+    if(mouse.mouseMove && mouse.drawing === false){
+        const x = e.clientX - mouse.x;
+        const y = e.clientY - mouse.y;
+        window.scrollTo(Globais.resRight - x, Globais.resTop - y, {behavior: 'smooth'});
+    }
+  })
+  // function for first load center screen
+    window.addEventListener('load', (e)=>{
+       console.log(Tela_05)
+    })
 
-        }) */
-         
+
+
     
 
-    window.addEventListener('resize', showButton);
-   
     
     return (
         <>
