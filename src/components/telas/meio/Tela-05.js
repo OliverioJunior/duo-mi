237086ee33275159d7625b8f5559edc4f54ed9e8
Oliverio <oliverio.junior2@gmail.com>
2022-03-28 07:09:10 -0300

    tirar coisas desnecessárias

--- src/components/telas/meio/Tela-05.js
+++ src/components/telas/meio/Tela-05.js
@@ -1,32 +1,11 @@
 import React from "react"
 import "./Tela-05.css"
 import { Navbar } from "../../Navbar"
-import Globais from "../../Globais"
-
-
-
 
 
 export default  function Tela_05(props){
 
 
-  const menu = document.querySelectorAll('.meio')
-  let res = 0
- menu.forEach((i) => {
-    res = i
-  })
-  Globais.resLeft =  res.offsetLeft
-  Globais.resTop = res.offsetTop
-
-  window.addEventListener('load', ()=>{
-    console.log('load')
-  
-    window.scrollTo(
-        {top: Globais.resTop,
-        left: Globais.resLeft,
-        behavior: 'smooth'}
-    )
-}) 
 
   return (
     <div className="meio">
