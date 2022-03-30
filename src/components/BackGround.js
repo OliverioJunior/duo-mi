2086346c825e68a55de09247a2e341afef3ba93e
Oliverio <oliverio.junior2@gmail.com>
2022-03-29 21:15:50 -0300

    mudar tamanho da tela e adicionar imagem

--- src/components/BackGround.js
+++ src/components/BackGround.js
@@ -2,11 +2,11 @@
 import React from 'react';
 import './BackGround.css';
 import { Tela_01, Tela_02, Tela_03, Tela_07, Tela_09 } from './telas/index.js';
-import  Tela_05  from './telas/meio/Tela-05';
+import Tela_05 from './telas/meio/Tela-05';
 import Globais from './Globais';
 
 
-function  BackGround() {
+function BackGround() {
     const body = document.querySelector('body');
     const mouse = {
         x: 0,
@@ -16,74 +16,66 @@ function  BackGround() {
             x: 0,
             y: 0
         }
-     };
-  window.addEventListener('mousedown',(e)=>{
-      if(Globais.setDrawing === false){
-            Globais.resRight =  window.scrollX;
-            Globais.resTop = window.scrollY;
-            mouse.x = e.clientX;
-            mouse.y = e.clientY;
-            mouse.mouseMove = true;
-            body.style.cursor = 'grabbing';
-    }
-  })
-  window.addEventListener('mouseup',()=>{
-      if(Globais.setDrawing === false){
-            mouse.mouseMove = false;
-            mouse.savePosition.x = window.scrollX;
-            mouse.savePosition.y = window.scrollY;
-            body.style.cursor = 'grab';
-    }
-  })
-  window.addEventListener('mousemove',(e)=>{
-      //to take the mouse position
-      //prevent mouse comportament 
-    e.preventDefault();
-    if(mouse.mouseMove && Globais.setDrawing === false){
-        const x = e.clientX - mouse.x;
-        const y = e.clientY - mouse.y;
-        window.scrollTo(Globais.resRight - x, Globais.resTop - y, {behavior: 'smooth'});
-    }
-  })
-
-  
-  
-
-  
-
-  
-
-
-
+    };
     
-
-    
-    return (
-        <>
+         window.addEventListener('mousedown', (e) => {
+            if (Globais.setDrawing === false) {
+                Globais.resRight = window.scrollX;
+                Globais.resTop = window.scrollY;
+                mouse.x = e.clientX;
+                mouse.y = e.clientY;
+                mouse.mouseMove = true;
+                body.style.cursor = 'grabbing';
+            }
            
-            <div className='back-ground'>
+        })
+        window.addEventListener('mouseup', (e) => {
+            if (Globais.setDrawing === false) {
+                mouse.mouseMove = false;
+                mouse.savePosition.x = window.scrollX;
+                mouse.savePosition.y = window.scrollY;
+                body.style.cursor = 'grab';
+            }
+          
+        })
+        window.addEventListener('mousemove', (e) => {
+            //to take the mouse position
+            //prevent mouse comportament 
+            e.preventDefault();
+            if (mouse.mouseMove && Globais.setDrawing === false) {
+                const x = e.clientX - mouse.x;
+                const y = e.clientY - mouse.y;
+                window.scrollTo(Globais.resRight - x, Globais.resTop - y, { behavior: 'smooth' });
+            }
+          
+        })
 
-                <Tela_01 />
-                <div className="cantoMeioEsquerdo" />
-                <Tela_07 />
-                <Tela_02 />
-                <Tela_05 className='firstLoad'/>
+        return (
+            <>
+
+                <div className='back-ground'>
+
+                    <Tela_01 />
+                    <div className="cantoMeioEsquerdo" />
+                    <Tela_07 />
+                    <Tela_02 />
+                    <Tela_05 className='firstLoad' />
 
 
 
-                <div className="meioInferior">
+                    <div className="meioInferior">
 
+                    </div>
+                    <Tela_03 />
+                    <div className="cantoMeioDireito">
+
+                    </div>
+                    <Tela_09 />
                 </div>
-                <Tela_03 />
-                <div className="cantoMeioDireito">
-
-                </div>
-                <Tela_09 /> 
-            </div>
 
 
-        </>
-    )
-}
+            </>
+        )
+    }
 
-export default BackGround;
+    export default BackGround;
