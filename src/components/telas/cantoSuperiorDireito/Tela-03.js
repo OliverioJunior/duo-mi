02a534d640b2da65b4246e2ab2621b824832da5c
Oliverio <oliverio.junior2@gmail.com>
2022-03-31 11:47:18 -0300

    adicionar link formulário

--- src/components/telas/cantoSuperiorDireito/Tela-03.js
+++ src/components/telas/cantoSuperiorDireito/Tela-03.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import "./Tela-03.css";
 
-function Tela_03(){
-    return(
+function Tela_03() {
+   
+    return (
         <div id='atendimento' className='cantoSuperiorDireito'>
             <h1>Acesse nosso <span>formulário</span> de atendimento online!</h1>
-            <div className="fotoDeFundo"/>
+            <div className="fotoDeFundo">
+               
+                   <a  target ='_blank' rel="noreferrer"  href="https://docs.google.com/forms/d/e/1FAIpQLSdinWexp4OkzeHxAB7jMY6cDXFb0OiM9N8o7ApL1rXGgff-jQ/viewform">
+                       <img className = 'linkFor'  alt=""/>
+                    </a>
+               
+               
+            </div>
         </div>
     )
 }
