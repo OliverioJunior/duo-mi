7a68da12d224b40d6216dc129d2a7f8655f0ac79
Oliverio <oliverio.junior2@gmail.com>
2022-03-31 11:47:50 -0300

    corrigir className

--- src/components/telas/meioSuperior/Tela-02.js
+++ src/components/telas/meioSuperior/Tela-02.js
@@ -10,7 +10,7 @@ function Tela_02(){
            </div> 
            <div className="redesSociais">
                 <a  dangerouslySetInnerHTML={{ __html: '' }} href='https://www.instagram.com/duomi.estudiocriativo/' rel="noreferrer" target="_blank" className='instagram'></a>
-                <a dangerouslySetInnerHTML={{ __html: '' }} href='https://www.behance.net/estudioduomi?tracking_source=search_projects%7Cduomi%20' rel="noreferrer"  target ='_blank' class='behance'></a>
+                <a dangerouslySetInnerHTML={{ __html: '' }} href='https://www.behance.net/estudioduomi?tracking_source=search_projects%7Cduomi%20' rel="noreferrer"  target ='_blank' className='behance'></a>
                 <a dangerouslySetInnerHTML={{ __html: '' }} href='https://www.facebook.com/DuoM%C3%AD-Est%C3%BAdio-Criativo-105370734736231/' rel="noreferrer"  target ='_blank' className='facebook'></a>
                 <a dangerouslySetInnerHTML={{ __html: '' }} href='https://www.tiktok.com/@duomi.estudiocriativo' target ='_blank' rel="noreferrer"  className='tiktok'></a>
             </div>
