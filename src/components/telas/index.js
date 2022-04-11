1f1a2710c8eca2226d619fe5bcf10e5bdc2ab303
Oliverio <oliverio.junior2@gmail.com>
2022-04-11 14:34:09 -0300

    mudar nomes

--- src/components/telas/index.js
+++ src/components/telas/index.js
@@ -1,8 +1,8 @@
 import QuemSomos from "./QuemSomos/QuemSomos"
-import redesSociais from "./redesSociais/RedesSociais"
+import RedesSociais from "./redesSociais/RedesSociais"
 import Formulario from "./Formulario/Formulario";
 import Tela_05 from "./meio/Tela-05";
-import Criatividade from "./cantoInferiorEsquerdo/Criatividade";
+import Criatividade from "./Criatividade/Criatividade";
 import Portfolio from "./portfólio/Portfolio";
 
-export {QuemSomos, redesSociais, Formulario, Tela_05, Criatividade, Portfolio};
+export {QuemSomos, RedesSociais, Formulario, Tela_05, Criatividade, Portfolio};
