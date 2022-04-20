f8f3bccd78f847ab69c391af44254324f2066a9d
Oliverio <oliverio.junior2@gmail.com>
2022-04-20 20:56:51 -0300

    criar componente texto

--- src/components/telas/QuemSomos/QuemSomos.js
+++ src/components/telas/QuemSomos/QuemSomos.js
@@ -3,6 +3,7 @@ import FotoDupla from "./fotoDupla/FotoDupla";
 import FotoEmily from "./fotoEmily/FotoEmily";
 import FotoTamis from "./fotoTamis/FotoTamis";
 import EmilyNome from "./emilyNome/EmilyNome";
+import Texto from "./texto/Texto";
 import "./QuemSomos.css"
 import "./responsive.css"
 const QuemSomos = () => {
@@ -11,9 +12,7 @@ const QuemSomos = () => {
             <div id="quemsomos"  className=" Estatico  grid">
                 <FotoEmily/>
                 <EmilyNome/>
-                <div className='Texto' >
-                    <h1>Clique na <span>imagem</span> para conhecer essa <span>dupla</span></h1>
-                </div>
+                <Texto/>
                 <FotoDupla/>
                 <FotoTamis/>
              </div>
---
+++ src/components/telas/QuemSomos/texto/Texto.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Texto.css'
+export default function Texto() {
+  return (
+    <div className='Texto' >
+         <h1>Clique na <span>imagem</span> para conhecer essa <span>dupla</span></h1>
+    </div>
+  )
+}
