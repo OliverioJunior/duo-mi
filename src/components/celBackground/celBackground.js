6d9b3430c97fb7e1fda645b8fb56e9500b22f3d7
Oliverio <oliverio.junior2@gmail.com>
2022-04-20 20:58:21 -0300

    melhorar responsividade

--- src/components/celBackground/celBackground.js
+++ src/components/celBackground/celBackground.js
@@ -3,6 +3,8 @@ import "./celBackground.css";
 import { QuemSomos, RedesSociais, Meio, Formulario, Criatividade, Portfolio } from '../telas/index.js';
 import FotoEmily from "../telas/QuemSomos/fotoEmily/FotoEmily";
 import EmilyNome from "../telas/QuemSomos/emilyNome/EmilyNome";
+import Texto from "../telas/QuemSomos/texto/Texto";
+import FotoDupla from "../telas/QuemSomos/fotoDupla/FotoDupla";
 export default function celBackground() {
   return (
     <div className='celBackground'>
@@ -14,7 +16,11 @@ export default function celBackground() {
           <FotoEmily/>
           <div className='containerNomeEmily'>
             <EmilyNome/>
+            <Texto/>
           </div>   
+       </div>
+       <div className ='containerDuplaTamis'>
+            <FotoDupla/>
        </div>
      </div>
     </div>
